bf31c958f86dcb91f74d27bb3b7cfa4837292a56
Amandeep Rathee <arathee2@gmail.com>
2019-11-10 22:24:21 -0500

    add sort and reverse map feature

--- word2id.py
+++ word2id.py
@@ -1,9 +1,8 @@
-sentence = "Rules and models destroy genius and art."
-sentence = sentence.split(" ")
-print(sentence)
-
-def word2id(words):
-    '''create a word to id mapping of the given string'''
+def word2id(string, split_on = " ", reverse=False):
+    '''create a word to id mapping of the given list of words'''
+    
+    # split text
+    words = string.split(split_on)
     
     # create an empty list to store word as keys and ids as values
     word2id = {}
@@ -14,15 +13,28 @@ def word2id(words):
     # for each new word, assign an id 'i'
     for word in words:
         word = word.lower()
-        if word not in word2id.keys():
+        if words not in word2id.keys():
             i = i + 1
             word2id[word] = i
+    
+    # sort word2id dictionary by key
+    word2id = sorted(word2id.items(), key=lambda item: item[1], reverse=False)
+    
+    # reverse mapping: id2word
+    if reverse:
+        id2word = {value: key for key, value in word2id.items()}
+        sorted(id2word.items(), key=lambda item: item[1], reverse=False)
+        return id2word
+    
     return word2id
 
-word2id = word2id(string)
-word2id = sorted(word2id.items(), key=lambda item: item[1], reverse=False)
+
+sentence = "Rules and models destroy genius and art."
+
+# word to id mapping
+word2id = word2id(sentence)
 print(word2id)
 
-# reverse mapping: store id as key and word as value
-id2word = {value: key for key, value in word2id.items()}
+# id to word mapping
+id2word = word2id(sentence, reverse=True)
 print(id2word)
